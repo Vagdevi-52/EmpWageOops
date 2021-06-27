c8cfdc40fe61883e460385cabca8531d7f8decd6
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-27 04:39:37 -0700

    to check wage

--- EmpWageOops.java
+++ EmpWageOops.java
@@ -4,20 +4,29 @@ class EmpWageOops
    {
 	Attendance employee = new Attendance();
 	employee.isPresent();
-
-    }
-}
-class  Attendance
-{
-   int IS_FULL_TIME = 1;
-   double empCheck = Math.floor(Math.random() * 10) % 2;
-   void isPresent()
-   {
-       if ( empCheck == IS_FULL_TIME )
-	            System.out.println("Employee is Present !");
-	else
-	            System.out.println("Employee is Absent !");
    }
-
 }
-
+class Attendance extends Wage
+{
+	int IS_FULL_TIME = 1;
+    	double empCheck = Math.floor(Math.random() * 10) % 2;
+    	void isPresent()
+        {
+        if ( empCheck == IS_FULL_TIME )
+         {
+            System.out.println("Employee is Present ");
+	    System.out.println("Wage is "+dailyWage());
+         }
+        else
+            System.out.println("Employee is Absent ");
+        }
+}
+class Wage
+{
+   final int wagePerHr = 20;
+   final int workHr = 8;
+   int dailyWage()
+   {
+     return wagePerHr * workHr;
+   }
+}
