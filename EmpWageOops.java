937252589b4dabec91604f91a4c86ea3eec3ac74
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-27 06:38:15 -0700

    Added:monthly wage based on days and hours

--- EmpWageOops.java
+++ EmpWageOops.java
@@ -2,38 +2,43 @@ class EmpWageOops
 {
    public static void main(String[] args)
    {
-      Attendance employee = new Attendance();
-      employee.isPresent();
+		Attendance employee = new Attendance();
+		employee.isPresent();
    }
 }
 class Attendance extends Wage
 {
-    int check;
-    static int days = 0;
-    void isPresent()
-    {
-        int wage = 0;
-        int totalWage = 0;
-        while ( days != 20 )
+	int check;
+	static int days = 0;
+	static int workHrs = 0;
+	static int totalWorkHrs = 0;
+        void isPresent()
+      {
+         int wage = 0;
+         int totalWage = 0;
+         while ( days != 20 && totalWorkHrs <= 100 )
         {
-            days++;
-            check = (int)(Math.floor(Math.random() * 10) % 3);
-            switch(check)
-            {
-                case 0:
-                    wage = dailyWage(8);
-                    break;
-                case 1:
-                    wage = dailyWage(4);
-                    break;
-                default:
-                    wage = 0;
-                    break;
-            }
-            totalWage = totalWage + wage;
-        }
-       System.out.println("Total earning of employee for a month is "+totalWage);
-    }
+          days++;
+          System.out.println(days);
+          check = (int)(Math.floor(Math.random() * 10) % 3);
+          switch(check)
+          {
+            case 0:
+                workHrs = 8;
+                break;
+            case 1:
+                workHrs = 4;
+                break;
+            default:
+                workHrs = 0;
+                break;
+          }
+        totalWorkHrs = totalWorkHrs + workHrs;
+        wage = dailyWage(workHrs);
+        totalWage = totalWage + wage;
+       }
+        System.out.println("Total earning of employee for a month is "+totalWage);
+     }
 }
 class Wage
 {
