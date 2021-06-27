93d21471f1e94692aee06f9d6e267b8f67ed0eba
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-27 04:22:10 -0700

    To check present or absent

--- EmpWageOops.java
+++ EmpWageOops.java
@@ -1,8 +1,23 @@
 class EmpWageOops
 {
-  public static void main(String[] args)
-  {
-    System.out.println("welcome to employee wage computation program");
-  }
+   public static void main(String[] args)
+   {
+	Attendance employee = new Attendance();
+	employee.isPresent();
+
+    }
+}
+class  Attendance
+{
+   int IS_FULL_TIME = 1;
+   double empCheck = Math.floor(Math.random() * 10) % 2;
+   void isPresent()
+   {
+       if ( empCheck == IS_FULL_TIME )
+	            System.out.println("Employee is Present !");
+	else
+	            System.out.println("Employee is Absent !");
+   }
+
 }
 
