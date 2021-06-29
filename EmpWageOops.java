2e0d78e3e473ec101bcc175adcfff777772609b7
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-29 03:38:56 -0700

    Added:Employee Wage class methods

--- EmpWageOops.java
+++ EmpWageOops.java
@@ -1,33 +1,20 @@
-class EmpWageOops
+public class EmpWageOops
+{ 
+  public static final int IS_PART_TIME = 1;
+  public static final int IS_FULL_TIME = 2;
+  public static final int EMP_RATE_PER_HOUR = 20;
+  public static final int NUM_OF_WORKING_DAYS = 2;
+  public static final int MAX_HRS_IN_MONTH = 10;
+
+public static int computeEmpWage()
 {
-   public static final int IS_PART_TIME = 1;
-   public static final int IS_FULL_TIME = 2;
-   private final String company;
-   private final int empRatePerHour;
-   private final int numOfWorkingDays;
-   private final int maxHoursPerMonth;
-   private int totalEmpWage;
-
-public  EmpWageOops(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-{
-
-   this.company = company;
-   this.empRatePerHour = empRatePerHour;
-   this.numOfWorkingDays = numOfWorkingDays;
-   this.maxHoursPerMonth = maxHoursPerMonth;
- }
-
-public void computeEmpWage()
-{
-
   int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-  while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+  while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
   {
-
-    totalWorkingDays++;
-    int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-    switch (empCheck)
-    {
+     totalWorkingDays++;
+     int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+     switch (empCheck)
+     {
        case IS_PART_TIME:
        empHrs = 4;
        break;
@@ -36,29 +23,15 @@ public void computeEmpWage()
        break;
        default:
        empHrs = 0;
-    }
-
+     }
     totalEmpHrs += empHrs;
-    System.out.println("Day#: " + totalWorkingDays +" Emp Hr: " + empHrs);
+    System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
   }
-
-   totalEmpWage = totalEmpHrs * empRatePerHour;
- }
-
-@Override
-public String toString()
- {
-
-   return "Total Emp Wage for Company:" +company+" is: " + totalEmpWage;
- }
-
-public static void main(String[] args)
-{
-    EmpWageOops dMart = new EmpWageOops("DMart", 28, 2, 10);
-    EmpWageOops reliance = new EmpWageOops ("Reliance", 10, 4, 20); 
-    dMart.computeEmpWage();
-    System.out.println(dMart);
-    reliance.computeEmpWage();
-    System.out.println(reliance);
+    int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR; System.out.println("Total Emp Wage: " + totalEmpWage); return totalEmpWage;
 }
+public static void main(String[] args)
+ { 
+   computeEmpWage();
+
+ }
 }
